3ed8b976a596426f4af4a8f16caa37d849023b43
vladyatsuk <yatsuk.vladislav@gmail.com>
2023-04-04 01:29:11 +0300

    Add matrix mulitplication using numpy

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,3 +1,4 @@
+import numpy
 from fastapi import APIRouter
 
 router = APIRouter()
@@ -6,3 +7,11 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+@router.get('/matrixproduct')
+def matrix_product() -> dict:
+    matrix_a = numpy.random.rand(10, 10).tolist()
+    matrix_b = numpy.random.rand(10, 10).tolist()
+    product = numpy.dot(matrix_a, matrix_b).tolist()
+    return {"matrix_a": matrix_a, "matrix_b": matrix_b, "product": product}
